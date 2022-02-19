56eb84e8c553381271a0f50db262f28c1d76b75e
Ido Saban <sabanido.business@gmail.com>
2022-02-20 00:40:10 +0200

    [Booking Service] Added BookingController to the app's routes and added import for `reflect-metadata` for `class-transformer`

--- services/booking/src/app.ts
+++ services/booking/src/app.ts
@@ -1,6 +1,10 @@
+import 'reflect-metadata';
 import express, { Express, NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import createLogger from '@common/log';
+import BookingController from '@booking/booking.controller';
+import BookingService from '@booking/booking.service';
+import authenticateJWT from '@common/middleware/jwt-auth.middleware';
 import swaggerUi from 'swagger-ui-express';
 import openapiSpecification from '@config/openapi-spec';
 
@@ -10,6 +14,12 @@ export default function createApp(): Express {
   const app = express();
 
   app.use(express.json());
+
+  const bookingController = new BookingController(
+    new BookingService(),
+    authenticateJWT,
+  );
+  app.use('/booking', bookingController.createRouter());
 
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
