4101700ab45f3ce3a3940eccb6aff83e453794bd
Ido Saban <sabanido.business@gmail.com>
2022-03-04 00:13:10 +0200

    [Booking Service] Added the `BookingController` class

---
+++ services/booking/src/booking/booking.controller.ts
@@ -0,0 +1,100 @@
+import { StatusCodes } from 'http-status-codes';
+import { instanceToPlain } from 'class-transformer';
+import createLogger from '@common/log';
+import { RequestMethod, Controller } from '@common/components';
+import validateUUID from '@common/util/validate-uuid.middleware';
+import transformAndValidateBody from '@common/util/transform-and-validate-body.middleware';
+import BookingService from './booking.service';
+import CreateBookingDto from './dto/create-booking.dto';
+import UpdateBookingDto from './dto/update-booking.dto';
+import Booking from './entities/booking.entity';
+
+export default class BookingController extends Controller {
+  private readonly log = createLogger(__filename);
+
+  constructor(private readonly bookingService: BookingService) {
+    super();
+
+    this.registerHandler(
+      'create',
+      RequestMethod.POST,
+      '/',
+      async (req, res) => {
+        const bookingDto = await transformAndValidateBody(
+          req,
+          CreateBookingDto,
+        );
+        const booking = await this.create(bookingDto);
+        res
+          .status(StatusCodes.CREATED)
+          .send(instanceToPlain(booking, { exposeUnsetFields: false }));
+      },
+    );
+
+    this.registerHandler(
+      'find',
+      RequestMethod.GET,
+      '/:id',
+      async (req, res) => {
+        const id = validateUUID(req, 'id');
+        const booking = await this.find(id);
+        res
+          .status(StatusCodes.OK)
+          .send(instanceToPlain(booking, { exposeUnsetFields: false }));
+      },
+    );
+
+    this.registerHandler(
+      'update',
+      RequestMethod.PUT,
+      '/:id',
+      async (req, res) => {
+        const id = validateUUID(req, 'id');
+        const bookingDto = await transformAndValidateBody(
+          req,
+          UpdateBookingDto,
+        );
+        const booking = await this.update(id, bookingDto);
+        res
+          .status(StatusCodes.OK)
+          .send(instanceToPlain(booking, { exposeUnsetFields: false }));
+      },
+    );
+
+    this.registerHandler(
+      'cancel',
+      RequestMethod.POST,
+      '/:id/cancel',
+      async (req, res) => {
+        const id = validateUUID(req, 'id');
+        const booking = await this.cancel(id);
+        res
+          .status(StatusCodes.OK)
+          .send(instanceToPlain(booking, { exposeUnsetFields: false }));
+      },
+    );
+  }
+
+  public async create(bookingDto: CreateBookingDto): Promise<Booking> {
+    const booking = this.bookingService.create(bookingDto);
+    return booking;
+  }
+
+  public async find(id: string): Promise<Booking> {
+    const booking = await this.bookingService.find(id);
+    return booking;
+  }
+
+  public async update(
+    id: string,
+    bookingDto: UpdateBookingDto,
+  ): Promise<Booking> {
+    const booking = await this.bookingService.update(id, bookingDto);
+    return booking;
+  }
+
+  public async cancel(id: string): Promise<Booking> {
+    const booking = await this.bookingService.cancel(id);
+    return booking;
+  }
+}
