f99531fd40ad040f49ae247c486ac8a7c91db3c9
Ido Saban <sabanido.business@gmail.com>
2022-03-07 02:02:00 +0200

    [Booking Service] Removed the custom `Middleware` type in favor of express's handler types

--- services/booking/src/common/components/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from 'express';
-
-export type Middleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => any;
-
-export default Middleware;
