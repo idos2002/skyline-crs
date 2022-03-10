87734b8ceee36be1affaba9122e3e5e199679ec8
Ido Saban <sabanido.business@gmail.com>
2022-03-10 09:52:39 +0200

    [Booking Service] Updated `app.ts` to be async and inject missing dependencies for previous changes

--- services/booking/src/app.ts
+++ services/booking/src/app.ts
@@ -1,25 +1,52 @@
 import 'reflect-metadata';
 import express, { Express } from 'express';
 import 'express-async-errors';
+import { GraphQLClient } from 'graphql-request';
+import { mongoose } from '@typegoose/typegoose';
+import amqp from 'amqplib';
+import swaggerUi from 'swagger-ui-express';
+import config, { openapiSpecification } from '@config';
 import BookingController from '@booking/booking.controller';
 import BookingService from '@booking/booking.service';
-import swaggerUi from 'swagger-ui-express';
-import openapiSpecification from '@config/openapi-spec';
 import FlightsService from '@flights/flights.service';
 import defaultErrorHandler from '@common/defaults/default.error-handler';
+import TicketService from '@ticket/ticket.service';
+import EmailService from '@email/email.service';
+import jsonBodySyntaxErrorHandler from '@common/defaults/json-body-syntax.error-handler';
 
-export default function createApp(): Express {
+export default async function createApp(): Promise<Express> {
   const app = express();
 
   app.use(express.json());
 
-  const flightsService = new FlightsService();
-  const bookingService = new BookingService(flightsService);
-  const bookingController = new BookingController(bookingService);
+  const flightsGraphQLClient = new GraphQLClient(config().inventoryManagerUrl);
+  const flightsService = new FlightsService(flightsGraphQLClient);
+
+  const pnrMongooseConnection = mongoose.createConnection(config().pnrDbUri, {
+    authSource: 'admin',
+  });
+  const bookingService = new BookingService(
+    pnrMongooseConnection,
+    flightsService,
+  );
+
+  const amqpConnection = await amqp.connect(config().rabbitmqUri);
+  const amqpChannel = await amqpConnection.createConfirmChannel();
+  const ticketService = new TicketService(amqpChannel);
+  const emailService = new EmailService(amqpChannel);
+
+  const bookingController = new BookingController(
+    bookingService,
+    ticketService,
+    emailService,
+  );
+
   // TODO: Apply JWT authentication middleware to the booking controller
   app.use('/booking', bookingController.createRouter());
 
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+
+  app.use(jsonBodySyntaxErrorHandler());
 
   // Default error handler - should be registered last!
   app.use(defaultErrorHandler());
