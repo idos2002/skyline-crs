56dd85444b414d6217df09610fd0560a98a945b5
Ido Saban <sabanido.business@gmail.com>
2022-03-10 09:39:54 +0200

    [Booking Service] Added the ticket service

---
+++ services/booking/src/ticket/ticket.service.ts
@@ -0,0 +1,54 @@
+import { ConfirmChannel, Options } from 'amqplib';
+import createLogger from '@common/log';
+import config from '@config';
+
+export default class TicketService {
+  private readonly log = createLogger(__filename);
+
+  private readonly queueBookingOptions: Options.Publish = {
+    persistent: true,
+    contentType: 'application/json',
+    contentEncoding: 'utf-8',
+    type: config().ticketBookingRoutingKey,
+  };
+
+  constructor(private readonly amqpChannel: ConfirmChannel) {
+    this.amqpChannel.assertExchange(config().ticketExchangeName, 'topic');
+  }
+
+  public queueBooking(bookingId: string, retries = 10) {
+    const message = JSON.stringify({ bookingId });
+
+    const onConfirm = (err: any) => {
+      if (err && retries > 0) {
+        // Retry to queue if failed on previous attempt
+        this.queueBooking(bookingId, retries - 1);
+        return;
+      }
+
+      const logData = {
+        bookingId,
+        exchange: config().ticketExchangeName,
+        routingKey: config().ticketBookingRoutingKey,
+      };
+
+      if (err) {
+        this.log.error(
+          { err, ...logData },
+          'Could not queue booking to be ticketed',
+        );
+        return;
+      }
+
+      this.log.info(logData, 'Successfully queued booking to be ticketed');
+    };
+
+    this.amqpChannel.publish(
+      config().ticketExchangeName,
+      config().ticketBookingRoutingKey,
+      Buffer.from(message),
+      this.queueBookingOptions,
+      onConfirm,
+    );
+  }
+}
