e4fa11cc0e3c2dc92d1fa1e0e2c8ba81cebe6b76
Ido Saban <sabanido.business@gmail.com>
2022-03-10 09:47:16 +0200

    [Booking Service] Converted the base controller class to use the express types and added `addErrorHandler()`

--- services/booking/src/common/components/controller.ts
+++ services/booking/src/common/components/controller.ts
@@ -1,4 +1,4 @@
-import express, { Router, RequestHandler } from 'express';
+import express, { Router, RequestHandler, ErrorRequestHandler } from 'express';
 import { boundClass } from 'autobind-decorator';
 import { Params } from 'express-serve-static-core';
 
@@ -14,29 +14,25 @@ export enum RequestMethod {
   TRACE = 'trace',
 }
 
-export type Handler<
-  ReqParams extends Params = any,
-  ReqBody extends object = object,
-  ReqQuery = qs.ParsedQs,
-  Locals extends Record<string, any> = Record<string, any>,
-> = RequestHandler<ReqParams, any, ReqBody, ReqQuery, Locals>;
-
 export interface RouteInfo {
   path: string;
   method?: RequestMethod | RequestMethod[];
 }
 
-type AnyHandler = Handler<any, any, any, any>;
+type AnyRequestHandler = RequestHandler<any, any, any, any, any>;
 
 interface HandlerRouteInfo extends Required<RouteInfo> {
   name: string;
-  middleware: AnyHandler[];
-  handler: AnyHandler;
+  middleware: AnyRequestHandler[];
+  handler: AnyRequestHandler;
 }
+
+type AnyErrorHandler = ErrorRequestHandler<any, any, any, any, any>;
 
 @boundClass // FIXME: Check if working with Jest!
 export class Controller {
   private readonly _routes: HandlerRouteInfo[] = [];
+  private readonly _errorHandlers: AnyErrorHandler[] = [];
 
   public createRouter(): Router {
     const router = express.Router();
@@ -61,6 +57,10 @@ export class Controller {
       }
     }
 
+    // Add error handlers to router
+    for (const errorHandler of this._errorHandlers) {
+      router.use(errorHandler);
+    }
     return router;
   }
 
@@ -71,8 +71,8 @@ export class Controller {
     Locals extends Record<string, any> = Record<string, any>,
   >(
     middleware:
-      | Handler<ReqParams, ReqBody, ReqQuery, Locals>
-      | Handler<ReqParams, ReqBody, ReqQuery, Locals>[],
+      | RequestHandler<ReqParams, any, ReqBody, ReqQuery, Locals>
+      | RequestHandler<ReqParams, any, ReqBody, ReqQuery, Locals>[],
     handlerName?: string | string[],
     exclude = false,
   ) {
@@ -103,7 +103,14 @@ export class Controller {
     }
   }
 
-  // TODO: Add addErrorHandler public method (like filters in NestJS)
+  public addErrorHandler<
+    ReqParams extends Params = any,
+    ReqBody extends object = object,
+    ReqQuery = qs.ParsedQs,
+    Locals extends Record<string, any> = Record<string, any>,
+  >(handler: ErrorRequestHandler<ReqParams, any, ReqBody, ReqQuery, Locals>) {
+    this._errorHandlers.push(handler);
+  }
 
   protected registerHandler<
     ReqParams extends Params = any,
@@ -114,7 +121,7 @@ export class Controller {
     name: string,
     method: RequestMethod | RequestMethod[],
     path: string,
-    handler: Handler<ReqParams, ReqBody, ReqQuery, Locals>,
+    handler: RequestHandler<ReqParams, any, ReqBody, ReqQuery, Locals>,
   ) {
     this._routes.push({ name, method, path, middleware: [], handler });
   }
