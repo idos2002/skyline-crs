cf612855e6cb4c9f7edf57468cd304966622c56b
Ido Saban <sabanido.business@gmail.com>
2022-03-10 09:53:39 +0200

    [Booking Service] Updated `main.ts` to be async for updated `app.ts`

--- services/booking/src/main.ts
+++ services/booking/src/main.ts
@@ -3,8 +3,14 @@ import config from '@config';
 import createLogger from '@common/log';
 
 const log = createLogger(__filename);
-const app = createApp();
 
-app.listen(config().port, '0.0.0.0', () => {
-  log.info(`Listening for connections on 0.0.0.0:${config().port}`);
-});
+async function main() {
+  const log = createLogger(__filename);
+  const app = await createApp();
+
+  app.listen(config().port, '0.0.0.0', () => {
+    log.info(`Listening for connections on 0.0.0.0:${config().port}`);
+  });
+}
+
+main().catch((err) => log.error(err, 'An uncaught unknown error was thrown'));
