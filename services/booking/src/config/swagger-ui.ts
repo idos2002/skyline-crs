ce10b71e713555e97573e5154ac4acda618cf933
Ido Saban <sabanido.business@gmail.com>
2022-03-17 22:59:59 +0200

    [Booking Service] Updated Swagger UI page title

--- services/booking/src/config/swagger-ui.ts
+++ services/booking/src/config/swagger-ui.ts
@@ -1,6 +1,7 @@
 import { SwaggerUiOptions } from 'swagger-ui-express';
 
 const swaggerUiOptions: SwaggerUiOptions = {
+  customSiteTitle: 'Booking Service - Swagger UI',
   customCss: '.swagger-ui .topbar { display: none }',
   swaggerOptions: {
     operationsSorter: 'alpha',
