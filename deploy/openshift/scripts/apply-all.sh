8bc52a1bc4a5d61cbd003a1c41e49653ef47a4c2
Ido Saban <sabanido.business@gmail.com>
2022-05-19 17:02:58 +0300

    [OpenShift] Updated scripts

--- deploy/openshift/scripts/apply-all.sh
+++ deploy/openshift/scripts/apply-all.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$(dirname $(realpath $0))
+cd $SCRIPT_DIR/..
+
 oc apply -f config
 oc apply -f volumes
 oc apply -f apps
--- deploy/openshift/scripts/delete-all.sh
+++ deploy/openshift/scripts/delete-all.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$(dirname $(realpath $0))
+cd $SCRIPT_DIR/..
+
 oc delete -f apps
 oc delete -f volumes
 oc delete -f config
